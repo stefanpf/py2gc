f1d57ed8c3047e159c0acecf6a9a391adc8cec8b
stefanpf <mail@stefanpfeuffer.com>
2018-12-06 09:26:12 +0100

    Update py2gc.py
    Added argument -p for file path to api credentials.

--- py2gc/py2gc.py
+++ py2gc/py2gc.py
@@ -3,18 +3,24 @@
 # A small command line utility to add events to a Google Calendar.
 
 import argparse
-from parsers import parse_date, parse_location, parse_note, parse_time
+from parsers import parse_date, parse_location, parse_note, parse_time, parse_path
 from api_call import build_json, call_api
 
 
 def parse_arguments():
     p = argparse.ArgumentParser(description='''A script to add events
                                             to a Google calendar.''')
-    p.add_argument('-d', '--date', help='Date of the event, either as DDMM or DDMMYYYY.', required=True)
-    p.add_argument('-n', '--note', help='Description of the event.', required=True)
-    p.add_argument('-s', '--starttime', help='Start time of the event, as HHMM in 24hr format.')
-    p.add_argument('-e', '--endtime', help='End time of the event, as HHMM in 24hr format.')
+    p.add_argument('-d', '--date', help='''Date of the event, either as DDMM
+                   or DDMMYYYY.''', required=True)
+    p.add_argument('-n', '--note', help='Description of the event.',
+                   required=True)
+    p.add_argument('-s', '--starttime', help='''Start time of the event,
+                    as HHMM in 24hr format.''')
+    p.add_argument('-e', '--endtime', help='''End time of the event,
+                    as HHMM in 24hr format.''')
     p.add_argument('-l', '--location', help='Location of the event.')
+    p.add_argument('-p', '--path', help='''Path to your Google Calendar API
+                    credentials.''')
     return p.parse_args()
 
 
@@ -40,8 +46,13 @@ if __name__ == "__main__":
     else:
         location = None
 
+    if args.path:
+        credentials_path = parse_path(args.path)
+    else:
+        credentials_path = None
+
     if date and note:
         json_event = build_json(date, note, start_time, end_time, location)
-        call_api(json_event)
+        call_api(json_event, credentials_path)
     else:
         print('Please enter a valid date and note.')
