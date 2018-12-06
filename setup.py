b84683d7f9e3353b996c042980b6cf36111c78d3
stefanpf <mail@stefanpfeuffer.com>
2018-12-06 11:32:39 +0100

    Update setup.py
    Change version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='py2gc',
-    version='1.0',
+    version='1.0.1',
     description='Add events to a Google Calendar from the command line.',
     license="MIT",
     long_description=long_description,
